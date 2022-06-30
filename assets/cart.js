15e530b68c656ad7bec631a30257113e3624e931
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2022-06-30 14:19:55 +0100

    test template

--- assets/cart.js
+++ assets/cart.js
@@ -11,7 +11,8 @@ class CartRemoveButton extends HTMLElement {
 
 customElements.define('cart-remove-button', CartRemoveButton);
 
-class CartItems extends HTMLElement {
+class CartItems  
+extends HTMLElement {
   constructor() {
     super();
 
